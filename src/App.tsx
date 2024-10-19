c661eb57fdb1b7a5d6208cf95cc1a2bc8af39940
lgr <962215611@qq.com>
2024-10-20 01:04:05 +0800

    test build

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+    const [content, setContent] = useState('');
+    const container = useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getinfo = () => {
+        fetch('https://yacshy.github.io')
+            .then((res) => res.json())
+            .then((res: string) => {
+                setContent(res);
+                console.log(content);
+                if (container.current) {
+                    container.current.innerHTML = res;
+                }
+            });
+    };
+
+    getinfo();
+
+    return (
+        <>
+            <div className="text-3xl font-bold underline bg-red-600 text-white" ref={container}></div>
+        </>
+    );
 }
 
-export default App
+export default App;
